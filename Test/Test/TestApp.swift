3bbd52799a9c30fe303e7cf85c3e464b09936b71
Mini <minnnidev@gmail.com>
2023-07-12 20:50:53 +0900

    [Setting] project setting

---
+++ Test/Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by 김민 on 2023/07/12.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
